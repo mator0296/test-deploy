1cf588e87362311c547345f6daf628c9f56d91ad
Sarait Hernandez <shr@solidusgroup.co>
2020-12-22 17:03:35 -0400

    requested changes

--- mesada/payment/models.py
+++ mesada/payment/models.py
@@ -1,24 +1,33 @@
 from django.db import models
+from django_countries.fields import CountryField
+from ..account.models import User
 
 
 class paymentMethods(models.Model):
 
-    type_method = models.CharField(max_length=50)
-    exp_month = models.IntegerField()
-    exp_year = models.IntegerField()
-    # network
-    last_digits = models.CharField(max_length=4)
-    # fingerprint
-    verification_cvv = models.CharField(max_length=3)
-    # verification_avs
+    type = models.CharField(max_length=50)
+    exp_month = models.PositiveIntegerField(
+        min_value=1, max_value=12, null=True, blank=True
+    )
+    exp_year = models.PositiveIntegerField(
+        min_value=2000, max_value=2050, null=True, blank=True
+    )
+    network = models.CharField(max_length=50, blank=True)
+    last_digits = models.CharField(max_length=4, blank=True)
+    fingerprint = models.CharField(max_length=36, blank=True)
+    verification_cvv = models.CharField(max_length=50, blank=True)
+    verification_avs = models.CharField(max_length=50, blank=True)
     phonenumber = models.CharField(max_length=15)
-    email = models.EmailField(max_length=254)
-    name = models.CharField(max_length=100)
-    address_line_1 = models.CharField(max_length=200)
-    address_line_2 = models.CharField(max_length=200)
-    postal_code = models.CharField(max_length=10)
-    city = models.CharField(max_length=100)
-    district = models.CharField(max_length=100)
-    country_code = models.CharField(max_length=4)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    email = models.EmailField(max_length=256)
+    name = models.CharField(max_length=256)
+    address_line_1 = models.CharField(max_length=256, blank=True)
+    address_line_2 = models.CharField(max_length=256, blank=True)
+    postal_code = models.CharField(max_length=6, blank=True)
+    city = models.CharField(max_length=256, blank=True)
+    district = models.CharField(max_length=256, blank=True)
+    country_code = CountryField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="payment_methods"
+    )
