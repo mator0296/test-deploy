db0e9692e2d050afc49676fe70d20688776666d8
Sarait Hernandez <shr@solidusgroup.co>
2020-12-23 16:51:49 -0400

    add validations and magic methods

--- mesada/payment/models.py
+++ mesada/payment/models.py
@@ -1,33 +1,47 @@
 from django.db import models
 from django_countries.fields import CountryField
+from django.core.validators import MinValueValidator, MaxValueValidator
 from ..account.models import User
+from django.utils import timezone
 
 
 class paymentMethods(models.Model):
 
-    type = models.CharField(max_length=50)
+    type = models.CharField(max_length=50, blank=False)
     exp_month = models.PositiveIntegerField(
-        min_value=1, max_value=12, null=True, blank=True
+        validators=[MinValueValidator(1), MaxValueValidator(12)], null=True, blank=True
     )
     exp_year = models.PositiveIntegerField(
-        min_value=2000, max_value=2050, null=True, blank=True
+        validators=[MaxValueValidator(2050)], null=True, blank=True
     )
     network = models.CharField(max_length=50, blank=True)
     last_digits = models.CharField(max_length=4, blank=True)
     fingerprint = models.CharField(max_length=36, blank=True)
     verification_cvv = models.CharField(max_length=50, blank=True)
     verification_avs = models.CharField(max_length=50, blank=True)
-    phonenumber = models.CharField(max_length=15)
-    email = models.EmailField(max_length=256)
-    name = models.CharField(max_length=256)
+    phonenumber = models.CharField(max_length=15, blank=True)
+    email = models.EmailField(max_length=256, blank=True)
+    name = models.CharField(max_length=256, blank=True)
     address_line_1 = models.CharField(max_length=256, blank=True)
     address_line_2 = models.CharField(max_length=256, blank=True)
     postal_code = models.CharField(max_length=6, blank=True)
     city = models.CharField(max_length=256, blank=True)
     district = models.CharField(max_length=256, blank=True)
     country_code = CountryField()
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(default=timezone.now, editable=False)
     updated = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="payment_methods"
+        User,
+        on_delete=models.CASCADE,
+        related_name="payment_methods",
+        null=True,
+        blank=True,
     )
+
+    @property
+    def __str__(self):
+        if self.last_digits:
+            return f"{self.type} {self.user} {self.last_digits}"
+        return f"{self.type} {self.user}"
+
+    __hash__ = models.Model.__hash__
