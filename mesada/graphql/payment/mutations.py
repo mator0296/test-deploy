c91e2f0183ed8f6363c3c43f3aab951ac98de9a2
Gustavo <gustavo.mejia@novateva.com>
2020-12-28 13:06:46 -0400

    Added billing details to card input

--- mesada/graphql/payment/mutations.py
+++ mesada/graphql/payment/mutations.py
@@ -4,17 +4,27 @@ from ...payment import create_card
 from ..core.mutations import BaseMutation
 
 
+class BillingDetailsInput(graphene.InputObjectType):
+    name = graphene.String(description="Full name of the card holder", required=True)
+    city = graphene.String(required=True)
+    country = graphene.String(
+        description="Country portion of the address. Formatted as a two-letter country code",
+        required=True,
+    )
+    line1 = graphene.String(description="Line one of the street address", required=True)
+    line2 = graphene.String(description="Line two of the street address")
+    district = graphene.String(
+        description="Region portion of the address. If the country is US or Canada district is required and should use the two-letter code for the subdivision."
+    )
+    postal_code = graphene.String(description="ZIP code of the address", required=True)
+
+
 class CardInput(graphene.InputObjectType):
-    encrypted_data = graphene.String(
-        description="Card encrypted data",
-        required=True
-    )
+    encrypted_data = graphene.String(description="Card encrypted data", required=True)
     key_id = graphene.String(description="Encryption key", required=True)
-    exp_month = graphene.Int(
-        description="Card expiration month",
-        required=True
-    )
+    exp_month = graphene.Int(description="Card expiration month", required=True)
     exp_year = graphene.Int(description="Card expiration year", required=True)
+    billing_details = BillingDetailsInput(description="Card billing details")
 
 
 class CreateCard(BaseMutation):
@@ -29,7 +39,7 @@ class CreateCard(BaseMutation):
     @classmethod
     def perform_mutation(cls, _root, info, **data):
         obj = CreateCard()
-
+        print(data)
         ip_address = info.context.META.get("REMOTE_ADDR")
         session = info.context.session
         body = {}
