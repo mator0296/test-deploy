4a4d56634a537e9143fa465ed3f778e5082f26ca
Sarait Hernandez <shr@solidusgroup.co>
2021-01-04 15:00:18 -0400

    adding decorators

--- mesada/graphql/payment/schema.py
+++ mesada/graphql/payment/schema.py
@@ -1,24 +1,28 @@
 import graphene
 from graphene_django.types import ObjectType
-
+from ..core.auth import login_required
 from ...payment.models import PaymentMethods
 from .types import PaymentMethodType
 
 
 class PaymentMethodsQueries(ObjectType):
     paymentMethod = graphene.Field(PaymentMethodType, id=graphene.Int())
-    userPaymentMethods = graphene.List(PaymentMethodType, user_id=graphene.Int())
+    userPaymentMethods = graphene.List(
+        PaymentMethodType, user_id=graphene.Int()
+    )
 
-    def resolve_payment_method(self, info, **kwargs):
+    @login_required
+    def resolve_payment_method(self, _info, **kwargs):
         _id = kwargs.get("id")
 
-        if info.context.user.is_authenticated() and _id is not None:
+        if _id is not None:
             return PaymentMethods.objects.get(pk=_id)
 
         return None
 
-    def resolve_user_payment_methods(self, info, user_id):
-        if info.context.user.is_authenticated() and user_id is not None:
+    @login_required
+    def resolve_user_payment_methods(self, _info, user_id):
+        if user_id is not None:
             PaymentMethods.objects.filter(user__id=user_id)
 
         return None
