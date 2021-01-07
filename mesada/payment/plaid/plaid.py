50268e9126a86aa3a544349986ba0b6720e5d3f9
Gustavo <gustavo.mejia@novateva.com>
2021-01-07 19:22:56 -0400

    Switched from Plaid API to module integration.

--- mesada/payment/plaid/plaid.py
+++ mesada/payment/plaid/plaid.py
@@ -10,15 +10,14 @@ from django.conf import settings
 from plaid import Client as ClientPlaid
 from plaid.errors import PlaidError
 
-HEADERS = {"Content-Type": "application/json"}
+client = ClientPlaid(
+    client_id=settings.PLAID_CLIENT_ID,
+    secret=settings.PLAID_SECRET_KEY,
+    environment=settings.PLAID_ENVIRONMENT,
+)
 
 
 def processor_token_create(public_token, account_id):
-    client = ClientPlaid(
-        client_id=settings.PLAID_CLIENT_ID,
-        secret=settings.PLAID_SECRET_KEY,
-        environment=settings.PLAID_ENVIRONMENT,
-    )
     try:
         # Exchange the public token for a Plaid access token.
         exchange_token_response = client.Item.public_token.exchange(
@@ -46,11 +45,9 @@ def create_link_token(body):
     Args:
         body (dict): Request body.
     """
-    body["client_id"] = settings.PLAID_CLIENT_ID
-    body["secret"] = settings.PLAID_SECRET_KEY
+    try:
+        response = client.LinkToken.create(body)
 
-    url = f"{settings.PLAID_BASE_URL}/link/token/create"
-    response = requests.request("POST", url, headers=HEADERS, json=body)
-    response.raise_for_status()
-
-    return response.json()
+        return response
+    except PlaidError as e:
+        return None, e.code, e.message
