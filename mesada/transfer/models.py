19a6cf74b8bb4063f1e5c9a6d207b5d3a4b45510
Kevin Torres <kevin.torres@novateva.com>
2021-01-08 14:29:21 -0400

    add models circle transfer

--- mesada/transfer/models.py
+++ mesada/transfer/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from djmoney.models.fields import MoneyField
 
+from ..account.models import User
 # Create your models here.
 
 
@@ -20,10 +21,11 @@ class CircleTransferType(models.TextChoices):
 
 
 class CircleTransfer(models.Model):
-    """ Model of a circle transfer """
-
+    ''' Model of a circle transfer '''
     transfer_id = models.CharField(max_length=256, unique=True)
-    source_type = models.CharField(max_length=10, choices=CircleTransferType.choices)
+    source_type = models.CharField(
+        max_length=10, choices=CircleTransferType.choices
+    )
     source_id = models.CharField(max_length=10)
     destination_type = models.CharField(
         max_length=10, choices=CircleTransferType.choices
@@ -35,19 +37,24 @@ class CircleTransfer(models.Model):
         max_length=8, choices=CircleTransferStatus.choices, blank=False, null=False
     )
     create_date = models.DateField()
+    user_id = models.ForeignKey(
+        User, null=True, blank=True, on_delete=models.SET_NULL
+    )
 
     class Meta:
         ordering = ("pk",)
 
     @property
     def full_transfer(self):
-        return "%s %s %s %s %s %s %s %s %s" % (
+        return "%s %s %s %s %s %s %s %s %s %s" % (
+            self.transfer_id,
             self.source_type,
+            self.source_id,
             self.destination_type,
             self.destination_address,
             self.destination_chain,
             self.amount,
-            self.currency,
             self.status,
-            self.createDate,
+            self.create_date,
+            self.user_id,
         )
