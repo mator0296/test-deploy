989bbde43b19a98de1e9e2ed2043b17ea2718745
kevin torres <kevin.torres@novateva.com>
2021-01-08 11:04:17 -0400

    delete ip 127.0.0.1

--- mesada/settings.py
+++ mesada/settings.py
@@ -38,9 +38,7 @@ ALLOWED_HOSTS = [
     "usa.mesada.io",
     "localhost",
     "usa-production.mesada.io",
-    "mesada-test-provitional.eba-aw53wdis.us-east-1.elasticbeanstalk.com"
-    "testserver",
-    '127.0.0.1',
+    "mesada-test-provitional.eba-aw53wdis.us-east-1.elasticbeanstalk.com" "testserver",
 ]
 
 AUTH_USER_MODEL = "account.User"
