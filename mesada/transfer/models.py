119a1beed47191b2c0ed20cd67f8f4d80322a86c
kevin torres <kevin.torres@novateva.com>
2021-01-08 11:57:26 -0400

    add models

--- mesada/transfer/models.py
+++ mesada/transfer/models.py
@@ -1,33 +1,53 @@
 from django.db import models
-
-# Create your models here.
-
-from django.db import models
 from djmoney.models.fields import MoneyField
+
 # Create your models here.
+
+
+class CircleTransferStatus(models.TextChoices):
+    """ State of the transfer in Circle """
+
+    PENDING = "pending"
+    COMPLETE = "complete"
+    FAILED = "failed"
+
+
+class CircleTransferType(models.TextChoices):
+    """ Circle transfer type """
+
+    BLOCKCHAIN = "blockchain"
+    WALLET = "wallet"
 
 
 class CircleTransfer(models.Model):
+    """ Model of a circle transfer """
 
-    transfer_id = models.CharField(max_length=256,
-                                   blank=False, null=False)
-    source_type = models.CharField(max_length=256, blank=False, null=False)
-    source_id = models.CharField(max_length=256, blank=False, null=False)
+    transfer_id = models.CharField(max_length=256, unique=True)
+    source_type = models.CharField(max_length=10, choices=CircleTransferType.choices)
+    source_id = models.CharField(max_length=10)
     destination_type = models.CharField(
-        max_length=256, blank=False, null=False)
-    destination_address = models.CharField(
-        max_length=256, blank=False, null=False)
-    destination_chain = models.CharField(
-        max_length=256, blank=False, null=False)
-    amount = MoneyField(max_digits=19, decimal_places=4,
-                        default_currency="USD")
-    currency = models.CharField(max_length=256, blank=False, null=False)
-    status = models.CharField(max_length=256, blank=False, null=False)
-    create_date = models.DateTimeField(null=False, blank=False)
+        max_length=10, choices=CircleTransferType.choices
+    )
+    destination_address = models.CharField(max_length=256)
+    destination_chain = models.CharField(max_length=4)
+    amount = MoneyField(max_digits=19, decimal_places=4, default_currency="USD")
+    status = models.CharField(
+        max_length=8, choices=CircleTransferStatus.choices, blank=False, null=False
+    )
+    create_date = models.DateField()
 
     class Meta:
         ordering = ("pk",)
 
     @property
     def full_transfer(self):
-        return "%s %s %s %s %s %s %s %s %s" % (self.source_type, self.destination_type, self.destination_address, self.destination_chain, self.amount, self.currency, self.status, self.createDate)
+        return "%s %s %s %s %s %s %s %s %s" % (
+            self.source_type,
+            self.destination_type,
+            self.destination_address,
+            self.destination_chain,
+            self.amount,
+            self.currency,
+            self.status,
+            self.createDate,
+        )
