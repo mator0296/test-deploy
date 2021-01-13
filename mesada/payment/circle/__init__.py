7fb9be19e42a7540ca2ed7ba806d250b6156a260
Miguel Mendez <miguel.mendez@novateva.com>
2021-01-13 14:38:07 -0400

    Updated create_transfer_by_blockchain

--- mesada/payment/circle/__init__.py
+++ mesada/payment/circle/__init__.py
@@ -11,6 +11,7 @@ import requests
 from django.conf import settings
 from django.utils import dateparse
 from django.core.exceptions import ValidationError
+from mesada.transfer.models import CircleTransfer
 from ...core.utils import generate_idempotency_key
 
 HEADERS = {
@@ -55,3 +56,44 @@ def create_payment(body: dict):
     response.raise_for_status()
 
     return response.json().get("data")
+
+
+def create_transfer_by_blockchain(amount, user):
+    """ Create a transfer by blockchain within the Circle API
+    Args:
+        amount: Amount to transfer.
+        user: User to which the foreign key will be associated in the CreateTransfer model.
+    """
+    payload = {
+        "source": {"type": "wallet", "id": f"{settings.CIRCLE_WALLET_ID}"},
+        "destination": {
+            "type": "blockchain",
+            "address": f"{settings.BITSO_BLOCKCHAIN_ADDRESS}",
+            "chain": f"{settings.CIRCLE_BLOCKCHAIN_CHAIN}",
+        },
+        "amount": {"amount": "{:.2f}".format(amount), "currency": "USD"},
+        "idempotencyKey": generate_idempotency_key(),
+    }
+
+    url = f"{settings.CIRCLE_BASE_URL}/transfers"
+    response = requests.request("POST", url, headers=HEADERS, json=payload)
+    response.raise_for_status()
+
+    try:
+        transfer = CircleTransfer(
+            transfer_id = response.json()["data"]["id"],
+            source_type = response.json()["data"]["source"]["type"],
+            source_id = response.json()["data"]["source"]["id"],
+            destination_type = response.json()["data"]["destination"]["type"],
+            destination_address = response.json()["data"]["destination"]["address"],
+            destination_chain = response.json()["data"]["destination"]["chain"],
+            amount = (response.json()["data"]["amount"]["amount"],response.json()["data"]["amount"]["currency"]),
+            status = response.json()["data"]["status"],
+            create_date = dateparse.parse_datetime(response.json()["data"]["createDate"]),
+            user_id = user
+        )
+        transfer.save()
+    except:
+        raise ValidationError({"CircleTransfer": "Error in Circle response from transfer"})
+
+    return response.json()["data"]["id"]
