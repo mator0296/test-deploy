61379ae16339041aabeb0ea94d6b6b36782e37f1
daniel_novateva <daniel.varela@novateva.com>
2021-01-14 12:03:04 -0400

    Fixed Bitso credentials error

--- mesada/withdrawal/bitso/__init__.py
+++ mesada/withdrawal/bitso/__init__.py
@@ -1,10 +1,18 @@
+import json
+
 from bitso import Api as BitsoAPI
+from bitso.errors import ApiError
 from django.conf import settings
 
 
-def make_bitso_sei_withdrawal(clabe, first_name, last_name, amount):
-    bitso_api = BitsoAPI(settings.BITSO_API_KEY, settings.BITSO_SECRET)
-    withdrawal = bitso_api.spei_withdrawal(
-        amount=amount, first_names=first_name, last_names=last_name, clabe=clabe
-    )
-    return withdrawal
+def make_bitso_spei_withdrawal(clabe, first_name, last_name, amount):
+    try:
+        bitso_api = BitsoAPI(settings.BITSO_API_KEY, settings.BITSO_SECRET)
+        withdrawal = bitso_api.spei_withdrawal(
+            amount=amount, first_names=first_name, last_names=last_name, clabe=clabe
+        )
+        return withdrawal
+
+    except ApiError as e:
+        #error_code = json.loads(str(e).replace("'", "\""))["code"]
+        return None
