fef5f68821e77a6c64d319457792ed21860fd22e
Miguel Mendez <miguel.mendez@novateva.com>
2021-01-14 18:33:08 -0400

    Import CreateTransfer Model

--- mesada/transfer/tasks.py
+++ mesada/transfer/tasks.py
@@ -1,12 +1,11 @@
 from celery import shared_task
 
 from mesada.payment.circle import get_circle_transfer_status
-
+from .models import CircleTransfer
 
 @shared_task
 def check_transfer_status():
-    from .models import CircleTransfer
-    
+
     transfers = CircleTransfer.objects.filter(status="pending")
     for transfer in transfers:
         status = get_circle_transfer_status(transfer.transfer_id)
