880a5273e5a42df9a9328f0d52ac8054dae6d489
daniel_novateva <daniel.varela@novateva.com>
2021-01-15 12:17:38 -0400

    Added PR Changes

--- tests/api/test_bitso.py
+++ tests/api/test_bitso.py
@@ -1,4 +1,3 @@
-import unittest
 import pytest
 
 from unittest.mock import patch
@@ -6,22 +5,22 @@ from mesada.withdrawal.bitso import make_bitso_spei_withdrawal
 from bitso.errors import ApiError
 
 
-class TestBitsoIntegration(unittest.TestCase):
-    @pytest.mark.integration
-    @patch("mesada.withdrawal.bitso.bitso")
-    def test_bitso_spei_withdrawal_success(self, mock_api):
-        mock_api.spei_withdrawal.return_value = "Succesfull API call"
-        withdrawal = make_bitso_spei_withdrawal(
-            "012345678901234567", "Satoshi", "Nakamoto", "1.0"
-        )
-        assert withdrawal is not None
+@pytest.mark.integration
+@patch("mesada.withdrawal.bitso.bitso")
+def test_bitso_spei_withdrawal_success(mock_api):
+    mock_api.spei_withdrawal.return_value = "Succesfull API call"
+    withdrawal = make_bitso_spei_withdrawal(
+        "012345678901234567", "Satoshi", "Nakamoto", "1.0"
+    )
+    assert withdrawal is not None
 
-    @pytest.mark.integration
-    @patch("mesada.withdrawal.bitso.bitso")
-    def test_bitso_spei_withdrawal_failure(self, mock_api):
 
-        mock_api.Api.side_effect = ApiError("Test")
-        withdrawal = make_bitso_spei_withdrawal(
-            "012345678901234567", "Satoshi", "Nakamoto", "1.0"
-        )
-        assert withdrawal is None
+@pytest.mark.integration
+@patch("mesada.withdrawal.bitso.bitso")
+def test_bitso_spei_withdrawal_failure(mock_api):
+
+    mock_api.Api.side_effect = ApiError("Test")
+    withdrawal = make_bitso_spei_withdrawal(
+        "012345678901234567", "Satoshi", "Nakamoto", "1.0"
+    )
+    assert withdrawal is None
