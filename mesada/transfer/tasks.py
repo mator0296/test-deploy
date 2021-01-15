1d7b7df25f8cb377888eb3fa236716faf48996e7
Miguel Mendez <miguel.mendez@novateva.com>
2021-01-15 09:39:34 -0400

    Removed the print

--- mesada/transfer/tasks.py
+++ mesada/transfer/tasks.py
@@ -3,6 +3,7 @@ from celery import shared_task
 from mesada.payment.circle import get_circle_transfer_status
 from .models import CircleTransfer
 
+
 @shared_task
 def check_transfer_status():
     transfers = CircleTransfer.objects.filter(status="pending")
