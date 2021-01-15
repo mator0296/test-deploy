e4869c3347c371fe06a12c10d67f5316ff95eefd
Gustavo <gustavo.mejia@novateva.com>
2021-01-15 16:32:23 -0400

    Styling code

--- mesada/payment/circle/__init__.py
+++ mesada/payment/circle/__init__.py
@@ -95,8 +95,8 @@ def create_transfer_by_blockchain(amount, user):
     )
 
     return data["id"]
-  
-  
+
+
 def register_ach(payment_method):
     """Register an ACH payment within the Circle API.
 
@@ -121,8 +121,8 @@ def register_ach(payment_method):
     response.raise_for_status()
 
     return response.json().get("data")
-  
-  
+
+
 def get_circle_transfer_status(transfer_id):
     """
     Get the status of a transfer using a get request to the circle api
--- mesada/transfer/tasks.py
+++ mesada/transfer/tasks.py
@@ -1,7 +1,8 @@
 from celery import shared_task
 
-from mesada.payment.circle import get_circle_transfer_status
 from .models import CircleTransfer
+
+from mesada.payment.circle import get_circle_transfer_status
 
 
 @shared_task
@@ -9,6 +10,6 @@ def check_transfer_status():
     transfers = CircleTransfer.objects.filter(status="pending")
     for transfer in transfers:
         status = get_circle_transfer_status(transfer.transfer_id)
-        if status is not "pending":
+        if status is not "pending":  # noqa: F632
             transfer.status = status
             transfer.save()
