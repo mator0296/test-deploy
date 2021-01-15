5171c12286afe8463ee261f0d035f7cb88e2f0db
Miguel Mendez <miguel.mendez@novateva.com>
2021-01-15 14:18:00 -0400

    Get the status of confirmed payments

--- mesada/celery.py
+++ mesada/celery.py
@@ -5,7 +5,7 @@ import os
 from celery import Celery
 from django.conf import settings
 
-from mesada.payment.tasks import check_payment_status
+from mesada.payment.tasks import check_payment_status, check_payment_paid_status
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mesada.settings")
@@ -25,3 +25,9 @@ def setup_periodic_tasks(sender, **kwargs):
         check_payment_status.s(),
         name="check status every minute",
     )
+    sender.add_periodic_task(
+        settings.CELERY_CHECK_PAYMENT_PAID_STATUS,
+        check_payment_paid_status.s(),
+        name="check the status every day",
+    )
+    
--- mesada/payment/tasks.py
+++ mesada/payment/tasks.py
@@ -4,3 +4,14 @@ from celery import shared_task
 @shared_task
 def check_payment_status():
     print("Status Checked---- call api")
+
+
+@shared_task
+def check_payment_paid_status():
+    from .models import Payment as PaymentModel
+    payments = PaymentModel.objects.filter(status="confirmed")
+    for payment in payments:
+        status = get_payment_status(payment.payment_token)
+        if status == "paid":
+            payment.status = status
+            payment.save()
