be40288498b29c5f107b72520ed143f50d96539b
david suriel <dasugarc@gmail.com>
2021-01-18 10:13:32 -0600

    fix payment task

--- mesada/payment/tasks.py
+++ mesada/payment/tasks.py
@@ -1,8 +1,6 @@
 from ..celery import app
-from .models import Payment
 
 
 @app.task
 def check_payment_status():
-    payment = Payment.objects.first()
-    print("Status Checked---- call api {}".format(payment.pk))
+    print("Status Checked---- call api {}".format("test"))
