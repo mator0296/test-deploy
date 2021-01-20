f92e9b06528f6719c945c049f9d51bc5f711a78d
Miguel Mendez <miguel.mendez@novateva.com>
2021-01-20 11:25:51 -0400

    Fix in check_transfer_status

--- mesada/transfer/tasks.py
+++ mesada/transfer/tasks.py
@@ -7,7 +7,7 @@ from mesada.payment.circle import get_circle_transfer_status
 
 @app.task
 def check_transfer_status():
-    transfers = CircleTransfer.objects.filter(status="pending")
+    transfers = CircleTransfer.objects.filter(status=TransferStatus.PENDING)
     for transfer in transfers:
         status = get_circle_transfer_status(transfer.transfer_id)
         if status != TransferStatus.PENDING:
