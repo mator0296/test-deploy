84d779c0ff3863a2484ca9593ca7a718efbc5e5e
Kevin Torres <kevin.torres@novateva.com>
2021-01-21 16:02:05 -0400

    fixes model galactus

--- mesada/galactus/models.py
+++ mesada/galactus/models.py
@@ -1,15 +1,13 @@
 from django.db import models
 from . import GalactusStatus
 
-# Create your models here.
-
 
 class GalactusTransaction(models.Model):
 
     status = models.CharField(
         max_length=9, choices=GalactusStatus.choices, default=GalactusStatus.PENDING
     )
-    detail = models.JSONField()
+    response_data = models.JSONField()
 
     def __str__(self):
         return "%s %s" % (self.status, self.detail)
