91f2e5c41dc65e98d58b6efbebd86dd3bdbd6cf6
Kevin Torres <kevin.torres@novateva.com>
2021-01-21 11:56:37 -0400

    add model galactus

--- mesada/galactus/models.py
+++ mesada/galactus/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class GalactusTransaction(models.Model):
 
-    status = models.CharField(max_length=256, blank=False, null=True)
-    detail = models.CharField(max_length=256, blank=False, null=True)
+    status = models.CharField(max_length=256, blank=False, null=False)
+    detail = models.CharField(max_length=256, blank=False, null=False)
 
     def __str__(self):
         return "%s %s" % (self.status, self.detail)
