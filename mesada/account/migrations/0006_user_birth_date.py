def60ba077080e92f6bff96cfd8f269dfa7d9b05
Victor <rojasv907@gmail.com>
2021-01-22 11:18:08 -0400

    fix in order migration

--- mesada/account/migrations/0006_user_birth_date.py
+++ mesada/account/migrations/0006_user_birth_date.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [("account", "0001_initial")]
+    dependencies = [("account", "0005_auto_20210120_2241")]
 
     operations = [
         migrations.AddField(
