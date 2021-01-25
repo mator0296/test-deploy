80d2abfe436e4321fca545c09931dfabc681f779
daniel_novateva <daniel.varela@novateva.com>
2021-01-25 10:37:41 -0400

    Initial implementation of the checkout model

--- mesada/checkout/models.py
+++ mesada/checkout/models.py
@@ -1,1 +1,35 @@
+from django.db import models
+from djmoney.models.fields import MoneyField
+
+from ..account.models import Recipient, User
+
+from mesada.payment import PaymentMethodStatus
+
 # Create your models here.
+
+
+class Checkout(models.Model):
+    checkout_token = models.CharField(max_length=256, blank=True)
+    amount = MoneyField(max_digits=19, decimal_places=4, default_currency="USD")
+    fees = MoneyField(max_digits=19, decimal_places=4, default_currency="USD")
+    total_amount = MoneyField(max_digits=19, decimal_places=4, default_currency="USD")
+    recipient_amount = MoneyField(
+        max_digits=19, decimal_places=4, default_currency="USD"
+    )
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="checkout", null=True, blank=True
+    )
+    recipient = models.ForeignKey(
+        Recipient,
+        on_delete=models.CASCADE,
+        related_name="checkout",
+        null=True,
+        blank=True,
+    )
+    payment_method = models.CharField(
+        max_length=50,
+        choices=PaymentMethodStatus.choices,
+        default=PaymentMethodStatus.PENDING,
+    )
+    status = models.CharField()
+    active = models.BooleanField()
