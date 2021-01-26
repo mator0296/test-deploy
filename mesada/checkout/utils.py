6669cb02412485f6b850fcdde58a9caae625a8ee
daniel_novateva <daniel.varela@novateva.com>
2021-01-26 11:38:31 -0400

    Fixed math errors

--- mesada/checkout/utils.py
+++ mesada/checkout/utils.py
@@ -3,12 +3,12 @@ from django.conf import settings
 from .payment import PaymentMethodTypes
 
 
-def calculate_fees(initial_amount, payment_method):
-    if payment_method.type == PaymentMethodTypes.CARD:
-        circle_fee = initial_amount * settings.COMMISSION_FEE_DEBIT_CARD
-    elif payment_method.type == PaymentMethodTypes.ACH:
-        circle_fee = initial_amount * settings.COMMISSION_FEE_ACH
+def calculate_fees(initial_amount, payment_type):
+    if payment_type == PaymentMethodTypes.CARD:
+        circle_fee = initial_amount * (settings.COMMISSION_FEE_DEBIT_CARD / 100)
+    elif payment_type == PaymentMethodTypes.ACH:
+        circle_fee = initial_amount * (settings.COMMISSION_FEE_ACH / 100)
 
-    mesada_fee = initial_amount * settings.COMMISSION_FEE_MESADA
+    mesada_fee = initial_amount * (settings.COMMISSION_FEE_MESADA / 100)
 
     return initial_amount - (mesada_fee + circle_fee)
