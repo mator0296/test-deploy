99f5f6e7901990ef6ad260f78112bcc805803999
Victor <rojasv907@gmail.com>
2021-01-28 14:23:56 -0400

    changin in start server configuration

--- start_server.sh
+++ start_server.sh
@@ -15,11 +15,7 @@ tail -n 0 -f /var/log/*.log &
 
 echo Starting Server.
 exec gunicorn mesada.wsgi \
-        --name core \
         --bind 0.0.0.0:8000 \
-        --workers 3 \
-        --timeout 120 \
-        --worker-class gevent \
         --log-level=info \
         --log-file=/var/log/gunicorn.log \
         --access-logfile=/var/log/access.log \
