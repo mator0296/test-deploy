3bfebca560773edf8fa70f628700200ceca9bf94
Victor <rojasv907@gmail.com>
2021-01-28 10:50:58 -0400

    changin in start server configuration

--- start_server.sh
+++ start_server.sh
@@ -5,13 +5,12 @@ if [ -n "$1" ]; then
     exec "$@"
 fi
 
+echo starting logs
 
 # Prepare log files and start outputting logs to stdout
 touch /var/log/gunicorn.log
 touch /var/log/access.log
 tail -n 0 -f /var/log/*.log &
-
-python manage.py migrate &
 
 
 echo Starting Server.
